53482f476e62c048abf2ea7042d9d60d438b3cc2
dbz0423 <2569876934@qq.com>
2024-11-29 16:47:07 +0800

    first commit

---
+++ src/test/java/top/zhu/tcomadminapi/TcomAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zhu.tcomadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcomAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
